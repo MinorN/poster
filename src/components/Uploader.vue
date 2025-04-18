9c83c8557ad82fe81d70103fd4cf53926a35b113
祁嘉俊 <1481700559@qq.com>
2025-04-18 15:02:09 +0800

    feat: 完善upload功能

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="file-upload">
-    <button @click="triggerUpload">
-      <span v-if="fileStatus === 'loading'">正在上传</span>
-      <span v-else-if="fileStatus === 'success'">上传成功</span>
-      <span v-else-if="fileStatus === 'error'">上传失败</span>
+    <button @click="triggerUpload" :disabled="isUploading">
+      <span v-if="isUploading">正在上传</span>
       <span v-else>点击上传</span>
     </button>
     <input
@@ -12,14 +10,33 @@
       :style="{ display: 'none' }"
       @change="handleFileChange"
     />
+    <ul>
+      <li
+        v-for="file in uploadedFiles"
+        :key="file.uid"
+        :class="`uploader-file upload-${file.status}`"
+      >
+        <span class="filename">{{ file.name }}</span>
+        <button class="delete-icon" @click="removeFile(file.uid)">del</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue"
+import { computed, defineComponent, reactive, ref } from "vue"
 import axios from "axios"
+import { v4 as uuidv4 } from "uuid"
 
 type UploadStaus = "ready" | "loading" | "success" | "error"
+
+export interface UploadFile {
+  uid: string
+  size: number
+  name: string
+  status: UploadStaus
+  raw: File
+}
 
 export default defineComponent({
   props: {
@@ -32,11 +49,22 @@ export default defineComponent({
   setup(props, context) {
     const fileInput = ref<null | HTMLInputElement>(null)
     const fileStatus = ref<UploadStaus>("ready")
+    const uploadedFiles = ref<UploadFile[]>([])
+
+    const isUploading = computed(() => {
+      return uploadedFiles.value.some((file) => file.status === "loading")
+    })
 
     const triggerUpload = () => {
       if (fileInput.value) {
         fileInput.value.click()
       }
+    }
+
+    const removeFile = (id: string) => {
+      uploadedFiles.value = uploadedFiles.value.filter(
+        (file) => file.uid !== id
+      )
     }
 
     const handleFileChange = (e: Event) => {
@@ -46,6 +74,14 @@ export default defineComponent({
         const uploadedFile = files[0]
         const formData = new FormData()
         formData.append(uploadedFile.name, uploadedFile)
+        const fileObj = reactive<UploadFile>({
+          uid: uuidv4(),
+          size: uploadedFile.size,
+          name: uploadedFile.name,
+          status: "loading",
+          raw: uploadedFile,
+        })
+        uploadedFiles.value.push(fileObj)
         fileStatus.value = "loading"
         axios
           .post(props.action, formData, {
@@ -55,10 +91,17 @@ export default defineComponent({
           })
           .then((res: any) => {
             console.log(res.data)
+            fileObj.status = "success"
             fileStatus.value = "success"
           })
           .catch((err: any) => {
+            fileObj.status = "error"
             fileStatus.value = "error"
+          })
+          .finally(() => {
+            if (fileInput.value) {
+              fileInput.value.value = ""
+            }
           })
       }
     }
@@ -68,9 +111,81 @@ export default defineComponent({
       triggerUpload,
       fileStatus,
       handleFileChange,
+      uploadedFiles,
+      isUploading,
+      removeFile,
     }
   },
 })
 </script>
 
-<style></style>
+<style scoped>
+.upload-list {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+.upload-list li {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  font-size: 14px;
+  line-height: 1.8;
+  margin-top: 5px;
+  box-sizing: border-box;
+  border-radius: 4px;
+  min-width: 200px;
+  position: relative;
+  &:first-child {
+    margin-top: 10px;
+  }
+  .upload-list-thumbnail {
+    vertical-align: middle;
+    display: inline-block;
+    width: 70px;
+    height: 70px;
+    position: relative;
+    z-index: 1;
+    background-color: #fff;
+    object-fit: cover;
+  }
+  .file-icon {
+    svg {
+      margin-right: 5px;
+      color: rgba(0, 0, 0, 0.45);
+    }
+  }
+  .filename {
+    margin-left: 5px;
+    margin-right: 40px;
+  }
+  &.upload-error {
+    color: #f5222d;
+    svg {
+      color: #f5222d;
+    }
+  }
+  .file-status {
+    display: block;
+    position: absolute;
+    right: 5px;
+    top: 0;
+    line-height: inherit;
+  }
+  .delete-icon {
+    display: none;
+    position: absolute;
+    right: 7px;
+    top: 0;
+    line-height: inherit;
+    cursor: pointer;
+  }
+  &:hover {
+    background-color: #efefef;
+    .file-status {
+      display: none;
+    }
+    .delete-icon {
+      display: block;
+    }
+  }
+}
+</style>
